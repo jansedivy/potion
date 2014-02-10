02c6c6301bc61c67c0021bcbb881b652aff24bb5
Jan Sedivy <jansedivy@abdoc.net>
2014-02-10 20:12:58 +0100

    simplify particle example

--- examples/particles/app.js
+++ examples/particles/app.js
@@ -1,43 +1,5 @@
-var Particle = function(app) {
-  var Particle = function(x, y, dx, dy) {
-    this.x = x;
-    this.y = y;
-    this.r = Math.random() * 20 + 5;
-    this.dx = dx;
-    this.dy = dy;
-
-    var colors = ['#04819e', '#38b2ce', '#60b9ce', '#015367', '#ff9900'];
-    this.color = colors[Math.floor(colors.length * Math.random())];
-  };
-
-  Particle.prototype.update = function(time) {
-    this.dx = this.dx + (0 - this.dx) * time;
-    this.dy = this.dy + (0 - this.dy) * time;
-
-    this.r = this.r + (0 - this.r) * time;
-
-    if (this.r <= 0.5) {
-      app.particles.splice(app.particles.indexOf(this), 1);
-    }
-
-    this.x += this.dx * time;
-    this.y += this.dy * time;
-  };
-
-  Particle.prototype.render = function() {
-    app.video.ctx.beginPath();
-    app.video.ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
-    app.video.ctx.strokeStyle = this.color;
-    app.video.ctx.stroke();
-    app.video.ctx.closePath();
-  };
-
-  return Particle;
-};
-
-Potion.init(document.querySelector('.game'), {
+var app = Potion.init(document.querySelector('.game'), {
   start: function() {
-    this.Particle = Particle(this);
     this.particles = [];
     this.lastPosition = { x: null, y: null };
   },
@@ -51,7 +13,7 @@ Potion.init(document.querySelector('.game'), {
     if (this.lastPosition.x && this.lastPosition.y) {
       var dx = (x - this.lastPosition.x) * 20;
       var dy = (y - this.lastPosition.y) * 20;
-      this.particles.push(new this.Particle(x, y, dx, dy));
+      this.particles.push(new Particle(x, y, dx, dy));
     }
 
     this.lastPosition.x = this.input.mouse.position.x;
@@ -72,3 +34,36 @@ Potion.init(document.querySelector('.game'), {
     }
   }
 });
+
+var Particle = function(x, y, dx, dy) {
+  this.x = x;
+  this.y = y;
+  this.r = Math.random() * 20 + 5;
+  this.dx = dx;
+  this.dy = dy;
+
+  var colors = ['#04819e', '#38b2ce', '#60b9ce', '#015367', '#ff9900'];
+  this.color = colors[Math.floor(colors.length * Math.random())];
+};
+
+Particle.prototype.update = function(time) {
+  this.dx = this.dx + (0 - this.dx) * time;
+  this.dy = this.dy + (0 - this.dy) * time;
+
+  this.r = this.r + (0 - this.r) * time;
+
+  if (this.r <= 0.5) {
+    app.particles.splice(app.particles.indexOf(this), 1);
+  }
+
+  this.x += this.dx * time;
+  this.y += this.dy * time;
+};
+
+Particle.prototype.render = function() {
+  app.video.ctx.beginPath();
+  app.video.ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+  app.video.ctx.strokeStyle = this.color;
+  app.video.ctx.stroke();
+  app.video.ctx.closePath();
+};
