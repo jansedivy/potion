a28a01d89c30c4969dd47c817441dc0755ff3b8c
Jan Sedivy <jansedivy@abdoc.net>
2014-02-11 14:17:58 +0100

    add example of video mixins

---
+++ examples/video-mixins/app.js
@@ -0,0 +1,19 @@
+var VideoMixins = {
+  circle: function(x, y, r) {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+    this.ctx.closePath();
+  }
+};
+
+Potion.init(document.querySelector('.game'), {
+  start: function() {
+    this.video.include(VideoMixins);
+  },
+
+  render: function() {
+    this.video.ctx.fillStyle = 'black';
+    this.video.circle(this.width/2, this.height/2, 40);
+    this.video.ctx.fill();
+  }
+});
