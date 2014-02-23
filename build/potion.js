6b4bfed091d692a4c044b20bb65375c004983dc2
Jan Sedivy <jansedivy@abdoc.net>
2014-02-23 13:53:19 +0100

    build new version

--- build/potion.js
+++ build/potion.js
@@ -1,5 +1,5 @@
 /**
- * potion - v0.1.2
+ * potion - v0.1.3
  * Copyright (c) 2014, Jan Sedivy
  *
  * Compiled: 2014-02-23
@@ -183,21 +183,26 @@ Assets.prototype.createSound = function(path, callback) {
  * @param {function} callback - function that is called when everything is loaded
  */
 Assets.prototype.load = function(data, callback) {
-  this.soundsPath = data.soundsPath || this.soundsPath;
   this.assetsPath = data.assetsPath || this.assetsPath;
 
-  var soundsToLoad = data.sounds.length;
+  if (!data.sounds) {
+    callback();
+  } else {
+    this.soundsPath = data.soundsPath || this.soundsPath;
 
-  var soundLoaded = function() {
-    soundsToLoad -= 1;
-    if (soundsToLoad <= 0) {
-      callback();
+    var soundsToLoad = data.sounds.length;
+
+    var soundLoaded = function() {
+      soundsToLoad -= 1;
+      if (soundsToLoad <= 0) {
+        callback();
+      }
+    };
+
+    for (var i=0, len=data.sounds.length; i<len; i++) {
+      var path = data.sounds[i];
+      this.createSound(path, soundLoaded);
     }
-  };
-
-  for (var i=0, len=data.sounds.length; i<len; i++) {
-    var path = data.sounds[i];
-    this.createSound(path, soundLoaded);
   }
 };
 
