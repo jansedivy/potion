4ba7e61779ea27e6a373e19dac663b24a898f669
Jan Sedivy <jansedivy@abdoc.net>
2014-04-14 22:11:28 +0200

    fix bug with running callback in next tick

--- src/assets.js
+++ src/assets.js
@@ -12,7 +12,7 @@ var Assets = function() {
 Assets.prototype.onload = function(callback) {
   this.callback = callback;
   if (this.thingsToLoad === 0) {
-    setTimeout(function() { this.callback(); }, 0);
+    setTimeout(callback, 0);
   }
 };
 
