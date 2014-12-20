7ccb23ed3f4861d9f710fdf83423a9f00c4b86ac
Jan Sedivy <jansedivy@abdoc.net>
2014-12-20 20:14:26 +0100

    remove sprites and animations helper methods

--- src/video.js
+++ src/video.js
@@ -92,52 +92,6 @@ Video.prototype._applySizeToCanvas = function() {
 };
 
 /**
- * Draws image sprite into x a y position
- * @param {object} sprite - sprite data
- * @param {number} x - x position
- * @param {number} y - y position
- * @param {number} [offsetX] - image position offset x
- * @param {number} [offsetY] - image position offset y
- * @param {number} [w] - final rendering width
- * @param {number} [h] - final rendering height
- */
-Video.prototype.sprite = function(image, x, y, offsetX, offsetY, w, h) {
-  if (!this.ctx) { return; }
-
-  offsetX = offsetX || 0;
-  offsetY = offsetY || 0;
-
-  w = w || image.width;
-  h = h || image.height;
-
-  x = Math.floor(x);
-  y = Math.floor(y);
-
-  var drawWidth = w;
-  var drawHeight = h;
-
-  if (image.src.match(/@2x.png$/)) {
-    drawWidth /= 2;
-    drawHeight /= 2;
-  }
-
-  this.ctx.drawImage(image, image.x + offsetX, image.y + offsetY, w, h, x, y, drawWidth, drawHeight);
-};
-
-/**
- * Draw animatino at given location
- * @param {Animation} animation - Animation object
- * @param {number} x - x position
- * @param {number} y - y position
- */
-Video.prototype.animation = function(animation, x, y) {
-  if (!this.ctx) { return; }
-
-  this.sprite(animation.image, x, y, animation.offsetX, animation.offsetY, animation.width, animation.height);
-};
-
-
-/**
  * clear canvas screen
  */
 Video.prototype.clear = function() {
