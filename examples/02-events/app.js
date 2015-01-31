6ec44d41b26cd027da31630deb91fcb6d2fb828f
Jan Sedivy <jansedivy@abdoc.net>
2015-01-31 16:12:40 +0100

    redo events example

--- examples/02-events/app.js
+++ examples/02-events/app.js
@@ -1,19 +1,43 @@
 Potion.init(document.querySelector('.game'), {
-  init: function() {
-    this.points = [];
+  /**
+   * keyboard events
+   */
+  keydown: function(key) {
+    this.log('keydown key: ' + key);
   },
 
-  render: function() {
-    for (var i=0; i<this.points.length; i++) {
-      var point = this.points[i];
-      this.video.ctx.fillRect(point.x, point.y, 10, 10);
-    }
+  keyup: function(key) {
+    this.log('keyup key: ' + key);
+  },
+
+  keypress: function(key) {
+    this.log('keypress key: ' + key);
+  },
+
+  /**
+   * Mouse events
+   */
+  mousedown: function(x, y, button) {
+    this.log('mousedown x: ' + x + ', y: ' + y + ', button: ' + button);
+  },
+
+  mouseup: function(x, y, button) {
+    this.log('mouseup x: ' + x + ', y: ' + y + ', button: ' + button);
   },
 
   click: function(x, y, button) {
-    // button 0 is left click
-    if (button === 0) {
-      this.points.push({ x: x, y: y });
-    }
+    this.log('click x: ' + x + ', y: ' + y + ', button: ' + button);
+  },
+
+  mousemove: function(x, y) {
+    this.log('mouseup x: ' + x + ', y: ' + y);
+  },
+
+  // helper function (not defined in potion)
+  log: function(message) {
+    var li = document.createElement('li');
+    li.textContent = message;
+    var debug = document.querySelector('.debug');
+    debug.insertBefore(li, debug.firstChild);
   }
 });
