383385b1450014dc5da568497fdcda613489a1bb
Jan Sedivy <jansedivy@abdoc.net>
2015-01-31 13:32:11 +0100

    update mouse example for new version

--- examples/04-mouse/app.js
+++ examples/04-mouse/app.js
@@ -1,9 +1,21 @@
 Potion.init(document.querySelector('.game'), {
   render: function() {
+    if (this.input.mouse.isDown) {
+      this.video.ctx.fillStyle = '#8298a6';
+    } else {
+      this.video.ctx.fillStyle = '#B4CBD9';
+    }
+
+    this.video.ctx.fillRect(0, 0, this.width, 50);
+
     var color = 'black';
 
-    if (this.input.mouse.isDown) {
-      color = 'red';
+    if (this.input.mouse.isLeftDown) {
+      color = '#048ABF';
+    } else if (this.input.mouse.isMiddleDown) {
+      color = '#03738C';
+    } else if (this.input.mouse.isRightDown) {
+      color = '#F2B366';
     }
 
     this.video.ctx.fillStyle = color;
