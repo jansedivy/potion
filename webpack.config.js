0e00ffa590c09774c6940045e50ca1746cedc6e1
Jan Sedivy <jansedivy@abdoc.net>
2015-04-02 13:36:17 +0200

    switch from Browserify/Grunt to Webpack build

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+];
+
+if (process.env.COMPRESS) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+if (process.env.COMPRESS) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+module.exports = {
+  output: {
+    library: 'Potion',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader' }
+    ]
+  },
+
+  node: {
+    Buffer: false
+  },
+
+  plugins: plugins
+};
