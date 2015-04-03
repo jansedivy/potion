d3c32088587f1726a84a5a4a20cf3d7f46573ffa
Jan Sedivy <jansedivy@abdoc.net>
2015-04-03 18:56:27 +0200

    change 00-setup example

--- examples/00-setup/app.js
+++ examples/00-setup/app.js
@@ -1,7 +1,11 @@
 var Potion = require('potion');
 
-Potion.init(document.querySelector('.game'), {
-  init: function() {
-    alert('potion init');
+var app = Potion.init(document.querySelector('.game'), {
+  render: function() {
+    app.video.ctx.fillStyle = 'black';
+    app.video.ctx.font = '20px sans-serif';
+    app.video.ctx.textAlign = 'left';
+    app.video.ctx.textBaseline = 'top';
+    app.video.ctx.fillText("Hello Potion", 10, 10);
   }
 });
