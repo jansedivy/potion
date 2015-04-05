250e96fd36b2364dec78c05809a3e0fbfbe5717c
Jan Sedivy <jansedivy@abdoc.net>
2015-04-06 01:13:01 +0200

    use "var app" style in keyboard example

--- examples/03-keyboard/app.js
+++ examples/03-keyboard/app.js
@@ -1,6 +1,6 @@
 var Potion = require('potion');
 
-Potion.init(document.querySelector('.game'), {
+var app = Potion.init(document.querySelector('.game'), {
   init: function() {
     this.x = 0;
     this.y = 0;
@@ -8,18 +8,18 @@ Potion.init(document.querySelector('.game'), {
   },
 
   update: function() {
-    if (this.input.isKeyDown('w')) { this.y -= 10; }
-    if (this.input.isKeyDown('d')) { this.x += 10; }
-    if (this.input.isKeyDown('s')) { this.y += 10; }
-    if (this.input.isKeyDown('a')) { this.x -= 10; }
+    if (app.input.isKeyDown('w')) { this.y -= 10; }
+    if (app.input.isKeyDown('d')) { this.x += 10; }
+    if (app.input.isKeyDown('s')) { this.y += 10; }
+    if (app.input.isKeyDown('a')) { this.x -= 10; }
   },
 
   render: function() {
     var color = 'black';
 
-    if (this.input.isKeyDown('shift')) { color = 'red'; }
+    if (app.input.isKeyDown('shift')) { color = 'red'; }
 
-    this.video.ctx.fillStyle = color;
-    this.video.ctx.fillRect(this.x, this.y, 40, 40);
+    app.video.ctx.fillStyle = color;
+    app.video.ctx.fillRect(this.x, this.y, 40, 40);
   }
 });
