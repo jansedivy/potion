f52acee9619542387e6926dc703ebcce4745ee15
Jan Sedivy <jansedivy@abdoc.net>
2015-04-05 13:51:37 +0200

    0.10.11

--- build/potion.js
+++ build/potion.js
@@ -297,8 +297,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 
-	var Video = __webpack_require__(9);
-	var Assets = __webpack_require__(10);
+	var Video = __webpack_require__(8);
+	var Assets = __webpack_require__(9);
 
 	/**
 	 * Game class that is subclassed by actual game code
@@ -540,7 +540,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 
-	var keys = __webpack_require__(8);
+	var keys = __webpack_require__(10);
 
 	/**
 	 * Input wrapper
@@ -793,6 +793,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        holder.state.enable();
 	      }
 	      holder.enabled = true;
+	      holder.changed = true;
 
 	      if (holder.paused) {
 	        this.unpause(name);
@@ -841,6 +842,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      holder.state.pause();
 	    }
 
+	    holder.changed = true;
 	    holder.paused = true;
 	  }
 	};
@@ -852,6 +854,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      holder.state.unpause();
 	    }
 
+	    holder.changed = true;
 	    holder.paused = false;
 	  }
 	};
@@ -957,17 +960,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	  for (var i = 0, len = this.updateOrder.length; i < len; i++) {
 	    var state = this.updateOrder[i];
 
-	    if (state && state.enabled) {
+	    if (state) {
 	      state.changed = false;
 
-	      if (!state.initialized && state.state.init) {
-	        state.initialized = true;
-	        state.state.init();
-	      }
+	      if (state.enabled) {
+	        if (!state.initialized && state.state.init) {
+	          state.initialized = true;
+	          state.state.init();
+	        }
 
-	      if (state.state.update && !state.paused) {
-	        state.state.update(time);
-	        state.updated = true;
+	        if (state.state.update && !state.paused) {
+	          state.state.update(time);
+	          state.updated = true;
+	        }
 	      }
 	    }
 	  }
@@ -1450,106 +1455,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 
-	module.exports = {
-	  MOUSE1: -1,
-	  MOUSE2: -3,
-	  MWHEEL_UP: -4,
-	  MWHEEL_DOWN: -5,
-	  BACKSPACE: 8,
-	  TAB: 9,
-	  ENTER: 13,
-	  PAUSE: 19,
-	  CAPS: 20,
-	  ESC: 27,
-	  SPACE: 32,
-	  PAGE_UP: 33,
-	  PAGE_DOWN: 34,
-	  END: 35,
-	  HOME: 36,
-	  LEFT: 37,
-	  UP: 38,
-	  RIGHT: 39,
-	  DOWN: 40,
-	  INSERT: 45,
-	  DELETE: 46,
-	  _0: 48,
-	  _1: 49,
-	  _2: 50,
-	  _3: 51,
-	  _4: 52,
-	  _5: 53,
-	  _6: 54,
-	  _7: 55,
-	  _8: 56,
-	  _9: 57,
-	  A: 65,
-	  B: 66,
-	  C: 67,
-	  D: 68,
-	  E: 69,
-	  F: 70,
-	  G: 71,
-	  H: 72,
-	  I: 73,
-	  J: 74,
-	  K: 75,
-	  L: 76,
-	  M: 77,
-	  N: 78,
-	  O: 79,
-	  P: 80,
-	  Q: 81,
-	  R: 82,
-	  S: 83,
-	  T: 84,
-	  U: 85,
-	  V: 86,
-	  W: 87,
-	  X: 88,
-	  Y: 89,
-	  Z: 90,
-	  NUMPAD_0: 96,
-	  NUMPAD_1: 97,
-	  NUMPAD_2: 98,
-	  NUMPAD_3: 99,
-	  NUMPAD_4: 100,
-	  NUMPAD_5: 101,
-	  NUMPAD_6: 102,
-	  NUMPAD_7: 103,
-	  NUMPAD_8: 104,
-	  NUMPAD_9: 105,
-	  MULTIPLY: 106,
-	  ADD: 107,
-	  SUBSTRACT: 109,
-	  DECIMAL: 110,
-	  DIVIDE: 111,
-	  F1: 112,
-	  F2: 113,
-	  F3: 114,
-	  F4: 115,
-	  F5: 116,
-	  F6: 117,
-	  F7: 118,
-	  F8: 119,
-	  F9: 120,
-	  F10: 121,
-	  F11: 122,
-	  F12: 123,
-	  SHIFT: 16,
-	  CTRL: 17,
-	  ALT: 18,
-	  PLUS: 187,
-	  COMMA: 188,
-	  MINUS: 189,
-	  PERIOD: 190
-	};
-
-/***/ },
-/* 9 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-
 	var isRetina = __webpack_require__(13)();
 
 	/**
@@ -1646,7 +1551,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = Video;
 
 /***/ },
-/* 10 */
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(process) {"use strict";
@@ -1840,6 +1745,106 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = Assets;
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(17)))
+
+/***/ },
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	module.exports = {
+	  MOUSE1: -1,
+	  MOUSE2: -3,
+	  MWHEEL_UP: -4,
+	  MWHEEL_DOWN: -5,
+	  BACKSPACE: 8,
+	  TAB: 9,
+	  ENTER: 13,
+	  PAUSE: 19,
+	  CAPS: 20,
+	  ESC: 27,
+	  SPACE: 32,
+	  PAGE_UP: 33,
+	  PAGE_DOWN: 34,
+	  END: 35,
+	  HOME: 36,
+	  LEFT: 37,
+	  UP: 38,
+	  RIGHT: 39,
+	  DOWN: 40,
+	  INSERT: 45,
+	  DELETE: 46,
+	  _0: 48,
+	  _1: 49,
+	  _2: 50,
+	  _3: 51,
+	  _4: 52,
+	  _5: 53,
+	  _6: 54,
+	  _7: 55,
+	  _8: 56,
+	  _9: 57,
+	  A: 65,
+	  B: 66,
+	  C: 67,
+	  D: 68,
+	  E: 69,
+	  F: 70,
+	  G: 71,
+	  H: 72,
+	  I: 73,
+	  J: 74,
+	  K: 75,
+	  L: 76,
+	  M: 77,
+	  N: 78,
+	  O: 79,
+	  P: 80,
+	  Q: 81,
+	  R: 82,
+	  S: 83,
+	  T: 84,
+	  U: 85,
+	  V: 86,
+	  W: 87,
+	  X: 88,
+	  Y: 89,
+	  Z: 90,
+	  NUMPAD_0: 96,
+	  NUMPAD_1: 97,
+	  NUMPAD_2: 98,
+	  NUMPAD_3: 99,
+	  NUMPAD_4: 100,
+	  NUMPAD_5: 101,
+	  NUMPAD_6: 102,
+	  NUMPAD_7: 103,
+	  NUMPAD_8: 104,
+	  NUMPAD_9: 105,
+	  MULTIPLY: 106,
+	  ADD: 107,
+	  SUBSTRACT: 109,
+	  DECIMAL: 110,
+	  DIVIDE: 111,
+	  F1: 112,
+	  F2: 113,
+	  F3: 114,
+	  F4: 115,
+	  F5: 116,
+	  F6: 117,
+	  F7: 118,
+	  F8: 119,
+	  F9: 120,
+	  F10: 121,
+	  F11: 122,
+	  F12: 123,
+	  SHIFT: 16,
+	  CTRL: 17,
+	  ALT: 18,
+	  PLUS: 187,
+	  COMMA: 188,
+	  MINUS: 189,
+	  PERIOD: 190
+	};
 
 /***/ },
 /* 11 */
