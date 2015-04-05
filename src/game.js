492e7dc4db613ae6536fd2220c8461ad849e1dc6
Jan Sedivy <jansedivy@abdoc.net>
2015-04-06 01:25:30 +0200

    remove unnecessary comments, its already in doc

--- src/game.js
+++ src/game.js
@@ -1,44 +1,20 @@
 var Video = require('./video');
 var Assets = require('./assets');
 
-/**
- * Game class that is subclassed by actual game code
- * @constructor
- * @param {HTMLCanvasElement} canvas - canvas DOM element
- */
 var Game = function(canvas) {
-  /**
-   * Canvas DOM element
-   * @type {HTMLCanvasElement}
-   */
   this.canvas = canvas;
 
-  /**
-   * Game width in pixels
-   * @type {number}
-   */
   this.width = 300;
 
-  /**
-   * Game highs in pixels
-   * @type {number}
-   */
   this.height = 300;
 
-  /**
-   * Instance of Assets for loading assets for the game
-   * @type {Assets}
-   */
   this.assets = new Assets();
 
   this.states = null;
-
   this.debug = null;
+  this.input = null;
+  this.video = null;
 
-  /**
-   * Object for configuring Potion
-   * @type {object}
-   */
   this.config = {
     useRetina: true,
     initializeCanvas: true,
@@ -52,16 +28,6 @@ var Game = function(canvas) {
 
   this.configure();
 
-  /**
-   * Input instance for mouse and keyboard events
-   * @type {Input}
-   */
-  this.input = null;
-
-  /**
-   * Video instance for rendering into canvas
-   * @type {Video}
-   */
   if (this.config.initializeVideo) {
     this.video = new Video(this, canvas, this.config);
   }
@@ -80,10 +46,6 @@ Game.prototype.setSize = function(width, height) {
   }
 };
 
-/**
- * Runs every frame in the loading phase. It's used for rendering the loading bar
- * @param {number} time - time in seconds since last frame
- */
 Game.prototype.preloading = function(time) {
   if (!this.config.showPreloader && !(this.video && this.video.ctx)) { return; }
 
