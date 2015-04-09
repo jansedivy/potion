ae965008e1cd90082763cacd94f8b6e6c8af4306
Jan Sedivy <jansedivy@abdoc.net>
2015-04-09 23:02:26 +0200

    build shared part for examples

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('shared.js'),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
