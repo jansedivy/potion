6d42db71d6988a0b52be50901d818facd6be81ec
Jan Sedivy <jansedivy@abdoc.net>
2015-04-11 20:20:46 +0200

    remove unused functions from utils file

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,3 @@
-var get = exports.get = function(url, callback) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-
-  request.onload = function() {
-    callback(this.response);
-  };
-
-  request.send();
-};
-
-var getJSON = exports.getJSON = function(url, callback) {
-  get(url, function(text) {
-    callback(JSON.parse(text));
-  });
-};
-
 exports.isFunction = function(obj) {
   return !!(obj && obj.constructor && obj.call && obj.apply);
 };
