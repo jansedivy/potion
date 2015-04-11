fa6ed2e1f83ccd2d729cfbba125b06c4102f9b3c
Jan Sedivy <jansedivy@abdoc.net>
2015-04-11 23:40:26 +0200

    add header comment in build files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 var webpack = require('webpack');
 
+var header = [
+  '/**',
+  '* potion - v' + require('./package.json').version,
+  '* Copyright (c) 2015, Jan Sedivy',
+  '*',
+  '* Potion is licensed under the MIT License.',
+  '*/'
+].join('\n');
+
 var plugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
@@ -15,6 +24,10 @@ if (process.env.COMPRESS) {
     })
   );
 }
+
+plugins.push(
+  new webpack.BannerPlugin(header, { raw: true })
+);
 
 module.exports = {
   output: {
