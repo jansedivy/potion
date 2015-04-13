5f496fd1cd5b711ea7a1d12f0f73001728991b77
Jan Sedivy <jansedivy@abdoc.net>
2015-04-13 23:08:29 +0200

    rewrite gravity in pixi

--- examples/demo-gravity/app.js
+++ examples/demo-gravity/app.js
@@ -1,3 +1,5 @@
+/* global PIXI */
+
 var Potion = require('potion');
 
 var app;
@@ -12,6 +14,12 @@ var Particle = function(x, y) {
   this.dy = Math.random() * 100 - 50;
 
   this.speed = 150;
+
+  this.object = new PIXI.Sprite(app.assets.get('particle.png'));
+
+  this.object.blendMode = PIXI.blendModes.SCREEN;
+
+  app.stage.addChild(this.object);
 };
 
 Particle.prototype.update = function(time) {
@@ -20,35 +28,45 @@ Particle.prototype.update = function(time) {
   this.dx += Math.cos(angle) * 5 * this.speed * time;
   this.dy += Math.sin(angle) * 5 * this.speed * time;
 
-  this.dx = this.dx + (0 - this.dx) * time/2;
-  this.dy = this.dy + (0 - this.dy) * time/2;
+  this.dx = this.dx + (0 - this.dx) * time/4;
+  this.dy = this.dy + (0 - this.dy) * time/4;
 
-  this.r = Math.sqrt(Math.pow(this.dx, 2) + Math.pow(this.dy, 2)) / 200 + 0.3;
+  this.r = Math.sqrt(Math.pow(this.dx, 2) + Math.pow(this.dy, 2)) / 200 + 0.2;
 
   this.x += this.dx * time;
   this.y += this.dy * time;
-};
 
-Particle.prototype.render = function() {
-  app.video.ctx.fillStyle = '#408655';
-  app.video.ctx.fillRect(this.x, this.y, this.r, this.r);
+  this.object.scale.set(this.r / 20);
+  this.object.position.x = this.x;
+  this.object.position.y = this.y;
 };
 
 app = Potion.init(document.querySelector('.game'), {
   configure: function() {
     this.setSize(document.body.clientWidth, document.body.clientHeight);
     this.config.allowHiDPI = false;
+    this.config.getCanvasContext = false;
 
+    this.renderer = new PIXI.WebGLRenderer(this.width, this.height, { resolution: this.config.allowHiDPI ? 2 : 1, view: this.canvas });
+
+    this.assets.addLoader('pixi', function(url, callback) {
+      this.assets._loaders.image(url, function(image) {
+        callback(new PIXI.Texture(new PIXI.BaseTexture(image)));
+      });
+    }.bind(this));
+
+    this.assets.load('pixi', 'particle.png');
+  },
+
+  init: function() {
     this.centerX = this.width/2;
     this.centerY = this.height/2;
 
     this.prevX = null;
     this.prevY = null;
-  },
 
-  init: function() {
-    // app.video.scaleCanvas(1/10);
     this.particles = [];
+    this.stage = new PIXI.Stage(0x080a25);
   },
 
   update: function(time) {
@@ -60,7 +78,7 @@ app = Potion.init(document.querySelector('.game'), {
     if (app.input.mouse.isDown) {
       for (var i=0; i<10; i++) {
         var angle = Math.random() * Math.PI*2;
-        var distance = Math.random() * 10 + 4;
+        var distance = Math.random() * 20 + 4;
 
         var particle = new Particle(Math.cos(angle) * distance + app.input.mouse.x, Math.sin(angle) * distance + app.input.mouse.y);
 
@@ -80,11 +98,6 @@ app = Potion.init(document.querySelector('.game'), {
   },
 
   render: function() {
-    app.video.ctx.fillStyle = '#408655';
-    app.video.ctx.fillRect(this.centerX - 5, this.centerY - 5, 10, 10);
-
-    for (var i=0, len=this.particles.length; i<len; i++) {
-      this.particles[i].render();
-    }
+    this.renderer.render(this.stage);
   }
 });
