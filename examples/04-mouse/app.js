bf3e119e2a53b6c0ef762aad6aa60801f8e0e07a
Jan Sedivy <jansedivy@abdoc.net>
2015-04-15 22:06:59 +0200

    add isActive to the mouse example

--- examples/04-mouse/app.js
+++ examples/04-mouse/app.js
@@ -23,7 +23,9 @@ var app = Potion.init(document.querySelector('.game'), {
       app.video.ctx.fillText('Right button is down', 10, 70);
     }
 
-    app.video.ctx.fillStyle = '#68B4FF';
-    app.video.ctx.fillRect(app.input.mouse.x - 15, app.input.mouse.y - 15, 30, 30);
+    if (app.input.mouse.isActive) {
+      app.video.ctx.fillStyle = '#68B4FF';
+      app.video.ctx.fillRect(app.input.mouse.x - 15, app.input.mouse.y - 15, 30, 30);
+    }
   }
 });
