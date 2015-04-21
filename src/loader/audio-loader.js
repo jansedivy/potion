34d02569addd68cc80e1c807c3a6580154a3762e
Jan Sedivy <jansedivy@abdoc.net>
2015-04-21 21:55:30 +0200

    add progress to audio

--- src/loader/audio-loader.js
+++ src/loader/audio-loader.js
@@ -1,7 +1,14 @@
 module.exports = function(manager) {
   return function(url, loader) {
-    manager.load(url, function(data) {
-      loader.done(data);
+    manager.load(url, {
+      done: function(data) {
+        loader.done(data);
+      },
+
+      progress: function(e) {
+        var percent = e.loaded / e.total;
+        loader.progress(percent);
+      }
     });
   };
 };
